0f34d7ce346e516a06e74d5658599eae83e72c09
Maxime Catteau <maxime.catteau.etu@univ-lille.fr>
2019-09-18 09:16:41 +0200

    add maven project

---
+++ TempProject/src/main/java/com/temp_project/app/App.java
@@ -0,0 +1,13 @@
+package com.temp_project.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
